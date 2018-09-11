124e427589cf5128631365bb90da325fe7bde0f1
Katrina Pritchard <pritckat@gmail.com>
2018-09-11 20:46:58 +0000

    automatically backed up by learn

--- app/models/heroes.rb
+++ app/models/heroes.rb
@@ -2,5 +2,16 @@ class Hero
 
   attr_accessor :name, :power, :bio
 
+  @@all = []
+
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
+  end
+
+  def self.all 
+    @@all 
+  end
   
 end
